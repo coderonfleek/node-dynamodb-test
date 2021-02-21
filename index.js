eebba3611fc21c0e94e16c0f4aff11d796bd36a0
Fikayo <fikfikky@gmail.com>
2021-02-21 21:53:36 +0100

    Using queries

--- index.js
+++ index.js
@@ -14,15 +14,16 @@ AWS.config.update({
 
 let docClient = new AWS.DynamoDB.DocumentClient();
 
-let table = "Tasks";
+let table = "Duties";
 
 let category = "Work";
 
 
-let params = {
+/* let params = {
     TableName: table,
     Key:{
-        "Category": category
+        "Category": category,
+        "Name" : "Name"
     }
 };
 
@@ -31,5 +32,27 @@ docClient.get(params, function(err, data) {
         console.error("Unable to read item. Error JSON:", JSON.stringify(err, null, 2));
     } else {
         console.log("GetItem succeeded:", JSON.stringify(data, null, 2));
+    }
+}); */
+
+var params = {
+    TableName : "Duties",
+    KeyConditionExpression: "#cat = :category",
+    ExpressionAttributeNames:{
+        "#cat": "Category"
+    },
+    ExpressionAttributeValues: {
+        ":category": "Work"
+    }
+};
+
+docClient.query(params, function(err, data) {
+    if (err) {
+        console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+    } else {
+        console.log("Query succeeded.");
+        data.Items.forEach(function(item) {
+            console.log(" -", item.Category + ": " + item.Name);
+        });
     }
 });
